1f445542a243b28f664b228448fd7f8565df86e4
박진수 <pajinsu0803@naver.com>
2025-05-12 23:01:05 +0900

    urls.py 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/login/', views.CookieTokenObtainPairView.as_view(), name='login'),
     path('api/refresh/', views.CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('api/password_change/', views.PasswordChangeAPIView.as_view(), name='password_change'),
+    path('api/delete/', views.DeleteAccountAPIView.as_view(), name='delete_account'),
 ]
