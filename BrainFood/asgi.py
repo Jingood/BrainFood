96140d67a3ea0fb9f796fbc7eb6414710a1c9391
박진수 <pajinsu0803@naver.com>
2025-05-27 00:02:19 +0900

    asgi.py 수정 / gunicorn 오류 해결

--- BrainFood/asgi.py
+++ BrainFood/asgi.py
@@ -12,11 +12,13 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrainFood.settings')
 
 django_asgi = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     'http': django_asgi,
