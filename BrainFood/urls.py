9aa44cacb1c22b4a858bc2ffd48bae0a85da0eda
박진수 <pajinsu0803@naver.com>
2025-05-28 18:04:19 +0900

    collectstatic dir 추가

--- BrainFood/urls.py
+++ BrainFood/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('chat/', include('chat.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
