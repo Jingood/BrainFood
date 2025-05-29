443838ccba17130b8783b83246ba7e9ce59bd5ae
박진수 <pajinsu0803@naver.com>
2025-05-29 22:35:48 +0900

    rest_framework 인증 방식 변경

--- BrainFood/settings.py
+++ BrainFood/settings.py
@@ -136,7 +136,6 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'accounts.authentication.CookieJWTAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
