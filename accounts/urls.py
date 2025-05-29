cbf1100a90b575b9577ecd740ac892200bd44328
박진수 <pajinsu0803@naver.com>
2025-05-29 22:49:13 +0900

    accounts/urls.py 로그아웃 url 연결

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('api/login/', views.CookieTokenObtainPairView.as_view(), name='login'),
     path('login/', views.LoginPageView.as_view(), name='t_login'),
     path('api/refresh/', views.CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/logout/', views.LogoutAPIView.as_view(), name="logout"),
     path('api/password_change/', views.PasswordChangeAPIView.as_view(), name='password_change'),
     path('api/delete/', views.DeleteAccountAPIView.as_view(), name='delete_account'),
     path('api/profile/', views.ProfileAPIView.as_view(), name='profile'),
